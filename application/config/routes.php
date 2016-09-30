761843348478cafbe989592f54b0d1bca0c48e30
CK Song <badukboy@gmail.com>
2016-09-30 14:26:45 -0700

    Completed #5

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,5 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
+$route['lock/(:any)/(:any)'] = 'welcome/shucks/$1';
+$route['show/(\d+)'] = 'first/gimme/3';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
